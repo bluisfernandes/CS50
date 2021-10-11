dd264da81eab1ac67aa0faa7b82c5f224cf96f82
Bruno Luis <bluisfernandes@gmail.com>
2021-10-11 06:21:33 +0000

    submit credit

--- pset6/credit/credit.py
+++ pset6/credit/credit.py
@@ -1,15 +1,44 @@
 from cs50 import get_string
+import sys
 
 
 def main():
     # prompts user to input credit card number
     number = get_string("Number: ")
 
-    # checks the company
-    check_company(number)
+    # checks if the number is valid
+    check_number(number)
+
+    # checks and prints the company
+    print_company(number)
 
 
-def check_company(number):
+# input with string
+def check_number(number):
+    luhn_aux = 0
+    luhn = 0
+
+    # adds 2x digits, beggining with the second-last
+    for i in range(-2, -len(number) - 1, -2):
+        luhn_aux = 2 * int(number[i])
+
+        # sum each digit
+        if luhn_aux >= 10:
+            luhn += luhn_aux % 10 + 1
+        else:
+            luhn += luhn_aux
+
+    # adds the other digits
+    for i in range(-1, -len(number) - 1, -2):
+        luhn += int(number[i])
+
+    # checks if the last digit is 0, if not, exit the program.
+    if luhn % 10 != 0:
+        print("INVALID")
+        sys.exit(0)
+
+
+def print_company(number):
     ''' AMEX numbers start with 34 or 37;
         MasterCard numbers start with 51, 52, 53, 54, or 55
         Visa numbers start with 4'''
