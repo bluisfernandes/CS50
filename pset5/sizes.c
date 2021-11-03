85d667290496e1b3aaee0d8669257b85f1fb48d8
Bruno Luis <bluisfernandes@gmail.com>
2021-11-03 22:04:02 +0000

    test

---
+++ pset5/sizes.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    printf("int=          %lu\n", sizeof(int));
+    printf("unsigned int= %lu\n", sizeof(unsigned int));
+    printf("long=         %lu\n", sizeof(long));
+    printf("double=       %lu\n", sizeof(double));
+    printf("long double=  %lu\n", sizeof(long double));
+    printf("long long=    %lu\n", sizeof(long long));
+    printf("char=         %lu\n", sizeof(char));
+    
+    
+    //printf("string=       %lu\n", sizeof(string));
+    
+    //printf("byte=         %lu\n", sizeof(BYTE));
+    
+    
+}
